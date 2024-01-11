80728039cfc02ec4f79c3407156a42563cdf8eed
aman <amanpachauria@gmail.com>
2024-01-12 03:35:15 +0530

    fix bug and add style

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -1,9 +1,9 @@
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Appbar() {
   const totalItems = useSelector((state) => state.cart.totalItems);
-  console.log("totalItem in appbar",totalItems);
+  console.log("totalItem in appbar", totalItems);
   return (
     <header className="fixed top-0 w-full bg-blue-200 border-blue-400 text-blue-800 z-50 opacity-97">
       <div className="flex justify-between items-center p-4 max-w-6xl mx-auto gap-1">
@@ -12,12 +12,14 @@ export default function Appbar() {
         </Link>
         <ul className="flex gap-5 font-semibold items-center">
           <Link to="/shopping-cart" className="relative hover:underline">
-            <span className='mr-2'>My Cart</span>
-            {totalItems > 0 && (
-              <span className="bg-green-500 text-white px-2 py-1 rounded-full absolute -top-1 -right-7">
-                {totalItems}
-              </span>
-            )}
+            <div>
+              <span className="mr-5">My Cart</span>
+              {totalItems > 0 && (
+                <span className="bg-green-500 mr-3 text-white px-2 py-1 rounded-full absolute -top-1 -right-7">
+                  {totalItems}
+                </span>
+              )}
+            </div>
           </Link>
         </ul>
       </div>
--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -40,20 +40,7 @@ export default function ListingItem({ product }) {
             </button>
           </Link>
           <div className="flex items-center gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 text-gray-500"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
+            { product.rating.rate <= 2.5 ? <span className="text-red-400">Rating</span> : <span className="text-green-700">Rating</span> }
             <span className="text-gray-500">{product.rating.rate}</span>
           </div>
         </div>
