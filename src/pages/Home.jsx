b422c4d7ab13a87120016a9af73c3cc47fffedd0
aman <amanpachauria@gmail.com>
2024-01-20 16:35:49 +0530

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
         } else {
           const response = await fetch(`https://fakestoreapi.com/products?limit=20&page=${page}`);
           if (response.ok === false) {
-            setError("Failed to fetch products");
+            setError("Failed to fetch  products");
             return;
           }
           const data = await response.json();
